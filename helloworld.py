fd6c8dc08a1205cd6c2b820d8f1189c6049f9e99
BartoszBury1 <91828936+BartoszBury1@users.noreply.github.com>
2022-03-27 13:10:51 +0200

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-print ("Hello World")
+from flask import Flask
+app = Flask(__hello__)
+  
+@app.route('/hello/<hello>')
+def hello_world(hello):
+   return 'Hello World'
+  
+if __hello__ == '__main__':
+   app.run()
